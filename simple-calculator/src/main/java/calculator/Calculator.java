8b9d6d61b6a7553e456551b5da4b30be8f4629cc
djmladenovic <166983269+djmladenovic@users.noreply.github.com>
2024-05-04 15:18:21 +0200

    Update Calculator.java

--- simple-calculator/src/main/java/calculator/Calculator.java
+++ simple-calculator/src/main/java/calculator/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
     
     public static int factorial(int a) {
         if (a == 1) {
-            return 1;
+            return 0; // namerna greska
         } else {
             return a * factorial(a - 1);
         }
